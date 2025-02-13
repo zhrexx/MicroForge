b03e0a820596f48d531da0b28f3fb097188e3d6c
dev <info@zhrxxgroup.com>
2025-02-13 16:10:54 +0100

    Big Update

--- HTTP/http.c
+++ HTTP/http.c
@@ -8,10 +8,14 @@
 #include <netinet/in.h>
 #include <arpa/inet.h>
 #include <time.h>
+#include <ctype.h> 
 
 #define PORT 8080
-#define BUFFER_SIZE 4096
 #define LOG_IP_ENABLED 1
+#define BUFFER_SIZE (6 * 1024 * 1024)
+#define MAX_LINES 1024
+#define MAX_TOKENS 256
+#define MAX_LENGTH 1024
 
 #ifdef SSL_ENABLE
 #include <openssl/ssl.h>
@@ -35,7 +39,84 @@ typedef struct {
     int param_count;
     char *host;
     char *body;
+    char *extracted_ip;
+    char *content_type;
 } HTTP_Request;
+
+char **blocklist = NULL;
+int block_count = 0;
+
+int load_blocklist(const char *filename) {
+    FILE *file = fopen(filename, "r");
+    if (!file) {
+        perror("Error opening file");
+        return -1;
+    }
+
+    blocklist = malloc(MAX_LINES * sizeof(char *));
+    if (!blocklist) {
+        perror("Memory allocation failed");
+        fclose(file);
+        return -1;
+    }
+
+    char buffer[MAX_LENGTH];
+    while (fgets(buffer, sizeof(buffer), file) && block_count < MAX_LINES) {
+        buffer[strcspn(buffer, "\n")] = 0;
+
+        char *token = strtok(buffer, " ");
+        while (token && block_count < MAX_LINES) {
+            blocklist[block_count] = strdup(token);
+            if (!blocklist[block_count]) {
+                perror("Memory allocation failed");
+                fclose(file);
+                return -1;
+            }
+            block_count++;
+            token = strtok(NULL, " ");
+        }
+    }
+
+    fclose(file);
+    return block_count;
+}
+
+void free_blocklist() {
+    for (int i = 0; i < block_count; i++) {
+        free(blocklist[i]);
+    }
+    free(blocklist);
+}
+
+
+const char* get_mime_type(const char *filename) {
+    const char *dot = strrchr(filename, '.');
+    if (!dot || dot == filename) return "application/octet-stream";
+    
+    char ext[32];
+    size_t i = 0;
+    dot++;
+    while (*dot && i < sizeof(ext) - 1) {
+        ext[i++] = tolower(*dot++);
+    }
+    ext[i] = '\0';
+    
+    if (strcmp(ext, "html") == 0) return "text/html";
+    if (strcmp(ext, "css") == 0) return "text/css";
+    if (strcmp(ext, "js") == 0) return "application/javascript";
+    if (strcmp(ext, "png") == 0) return "image/png";
+    if (strcmp(ext, "jpg") == 0 || strcmp(ext, "jpeg") == 0) return "image/jpeg";
+    if (strcmp(ext, "gif") == 0) return "image/gif";
+    if (strcmp(ext, "svg") == 0) return "image/svg+xml";
+    if (strcmp(ext, "ico") == 0) return "image/x-icon";
+    if (strcmp(ext, "pdf") == 0) return "application/pdf";
+    if (strcmp(ext, "txt") == 0) return "text/plain";
+    if (strcmp(ext, "xml") == 0) return "application/xml";
+    if (strcmp(ext, "json") == 0) return "application/json";
+    
+    return "application/octet-stream";
+}
+
 
 void log_msg(const char *prefix, const char *format, ...) {
     va_list args;
@@ -120,6 +201,16 @@ HTTP_Request parse_http_request(const char *request) {
     if (host_header) {
         result.host = str_dup_until(host_header + 6, '\n');
     }
+    const char *xff_header = strstr(request, "X-Forwarded-For: ");
+    if (xff_header) {
+        result.extracted_ip = str_dup_until(xff_header + 17, '\r');
+    }
+    else {
+        result.extracted_ip = strdup("NOTPROVIDED");
+    }
+
+    const char *content_type = strstr(request, "Content-Type: ");
+    result.content_type = strdup(content_type);
 
     if (result.method == HM_POST) {
         const char *body = strstr(request, "\r\n\r\n");
@@ -127,7 +218,6 @@ HTTP_Request parse_http_request(const char *request) {
             body += 4;
             result.body = strdup(body);
             
-            const char *content_type = strstr(request, "Content-Type: ");
             if (content_type && strstr(content_type, "application/x-www-form-urlencoded")) {
                 int count = 0;
                 for (const char *p = body; *p; p++) {
@@ -186,22 +276,104 @@ char *method_to_str(HTTP_Method method) {
     else return "Unknown";
 }
 
+void send_file_response(int client_socket, char *status, const char *filepath
 #ifdef SSL_ENABLE
-void handle_client(int client_socket, SSL_CTX *ctx) {
-#else
-void handle_client(int client_socket) {
+    , SSL *ssl
 #endif
+) {
+    FILE *fp = fopen(filepath, "rb");
+    if (!fp) {
+        char *not_found = "404 Not Found";
+#ifdef SSL_ENABLE
+        send_response(client_socket, "404 Not Found", not_found, ssl);
+#else
+        send_response(client_socket, "404 Not Found", not_found);
+#endif
+        return;
+    }
+
+    fseek(fp, 0, SEEK_END);
+    long file_size = ftell(fp);
+    fseek(fp, 0, SEEK_SET);
+    
+    const char *mime_type = get_mime_type(filepath);
+    
+    char header[512];
+    snprintf(header, sizeof(header), 
+        "HTTP/1.1 %s\r\n"
+        "Content-Type: %s\r\n"
+        "Content-Length: %ld\r\n"
+        "Cache-Control: public, max-age=31536000\r\n"
+        "\r\n", 
+        status, mime_type, file_size);
+        
+#ifdef SSL_ENABLE
+    SSL_write(ssl, header, strlen(header));
+#else
+    send(client_socket, header, strlen(header), 0);
+#endif
+
+    char *buffer = malloc(BUFFER_SIZE);
+    if (!buffer) {
+        fclose(fp);
+        return;
+    }
+
+    size_t bytes_read;
+    while ((bytes_read = fread(buffer, 1, BUFFER_SIZE, fp)) > 0) {
+#ifdef SSL_ENABLE
+        SSL_write(ssl, buffer, bytes_read);
+#else
+        send(client_socket, buffer, bytes_read, 0);
+#endif
+    }
+    
+    free(buffer);
+    fclose(fp);
+}
+
+int check_ip_address(char *ip) {
+    for (int i = 0; i < block_count; i++) {
+        if (strncmp(ip, blocklist[i], strlen(blocklist[i])) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void handle_client(int client_socket
+#ifdef SSL_ENABLE
+    , SSL_CTX *ctx
+#endif
+) {
     struct sockaddr_in client_addr;
     socklen_t client_len = sizeof(client_addr);
+    
+    
 
     if (getpeername(client_socket, (struct sockaddr *)&client_addr, &client_len) < 0) {
         perror("getpeername failed");
         close(client_socket);
-        exit(1);
+        return;
     }
     
     char *client_ip_address = inet_ntoa(client_addr.sin_addr);
     int client_port = ntohs(client_addr.sin_port);
+
+    time_t rawtime;
+    struct tm *timeinfo;
+    char time_str[20];
+    
+    time(&rawtime); 
+    timeinfo = localtime(&rawtime);
+    strftime(time_str, sizeof(time_str), "%d-%m %H:%M", timeinfo);
+    
+    if (check_ip_address(client_ip_address)) {
+        printf("[%s:%d %s] Blocked Connection: Banned IP address\n", client_ip_address, client_port, time_str);
+        send_response(client_socket, "404 BLOCKED_IP_ADDRESS", "Your IP address is blocked!");
+        close(client_socket);
+        return;
+    }
 
     char buffer[BUFFER_SIZE];
     ssize_t bytes_received;
@@ -221,22 +393,15 @@ void handle_client(int client_socket) {
 
     if (bytes_received <= 0) {
         perror("recv");
-        #ifdef SSL_ENABLE
+#ifdef SSL_ENABLE
         SSL_free(ssl);
-        #endif
+#endif
         close(client_socket);
         return;
     }
     
     buffer[bytes_received] = '\0';
     
-    time_t rawtime;
-    struct tm *timeinfo;
-    char time_str[20];
-    
-    time(&rawtime); 
-    timeinfo = localtime(&rawtime);
-    strftime(time_str, sizeof(time_str), "%d-%m %H:%M", timeinfo);
 
     HTTP_Request req = parse_http_request(buffer);
     if (LOG_IP_ENABLED) {
@@ -245,54 +410,27 @@ void handle_client(int client_socket) {
         printf("[%s] %s %s\n", time_str, method_to_str(req.method), req.route);
     }
 
-    if (check_route(req.route, "/")) {
-        FILE *fp = fopen("index.html", "r");
-        if (!fp) {
-            #ifdef SSL_ENABLE
-            send_response(client_socket, "404 NOT_FOUND", "File not found!", ssl);
-            #else
-            send_response(client_socket, "404 NOT_FOUND", "File not found!");
-            #endif
-        } else {
-            char content[1024];
-            size_t len = fread(content, 1, sizeof(content) - 1, fp);
-            content[len] = '\0';
-            fclose(fp);
-            #ifdef SSL_ENABLE
-            send_response(client_socket, "200 OK", content, ssl);
-            #else
-            send_response(client_socket, "200 OK", content);
-            #endif
-        }
-    } else if (check_route(req.route, "/server_info/") || check_route(req.route, "/server_info")) {
-        #ifdef SSL_ENABLE
+    char file_path[512];
+    if (strcmp(req.route, "/") == 0) {
+        strcpy(file_path, "index.html");
+    } else if (strcmp(req.route, "/server_info") == 0 || strcmp(req.route, "/server_info/") == 0) {
+#ifdef SSL_ENABLE
         send_response(client_socket, "200 OK", "<!DOCTYPE html>This is running on <a href=\"https://github.com/zhrexx/MicroForge/tree/main/HTTP\">MicroForge/HTTP</a> created by <a href=\"https://github.com/zhrexx\">zhrexx</a>", ssl);
-        #else
+#else
         send_response(client_socket, "200 OK", "<!DOCTYPE html>This is running on <a href=\"https://github.com/zhrexx/MicroForge/tree/main/HTTP\">MicroForge/HTTP</a> created by <a href=\"https://github.com/zhrexx\">zhrexx</a>");
-        #endif
+#endif
+        goto cleanup;
     } else {
-        char file_path[256];
-        snprintf(file_path, sizeof(file_path), "%s.html", req.route + 1);  
-        FILE *fp = fopen(file_path, "r");
-        if (!fp) {
-            #ifdef SSL_ENABLE
-            send_response(client_socket, "404 NOT_FOUND", "File not found!", ssl);
-            #else
-            send_response(client_socket, "404 NOT_FOUND", "File not found!");
-            #endif
-        } else {
-            char content[1024];
-            size_t len = fread(content, 1, sizeof(content) - 1, fp);
-            content[len] = '\0';
-            fclose(fp);
-            #ifdef SSL_ENABLE
-            send_response(client_socket, "200 OK", content, ssl);
-            #else
-            send_response(client_socket, "200 OK", content);
-            #endif
-        }
+        snprintf(file_path, sizeof(file_path), "%s", req.route + 1);
     }
 
+#ifdef SSL_ENABLE
+    send_file_response(client_socket, "200 OK", file_path, ssl);
+#else
+    send_file_response(client_socket, "200 OK", file_path);
+#endif
+
+cleanup:
     free(req.host);
     free(req.route);
     if (req.parameters) {
@@ -316,7 +454,10 @@ void handle_client(int client_socket) {
 int main() {
     int server_fd, client_socket;
     struct sockaddr_in server_addr;
-    
+    if (load_blocklist("BLOCKLIST") < 0) {
+        return 1;
+    } 
+
 #ifdef SSL_ENABLE 
     SSL_CTX *ctx;
     init_ssl();
@@ -383,5 +524,6 @@ int main() {
     SSL_CTX_free(ctx);
 #endif 
     close(server_fd);
+    free_blocklist();
     return 0;
 }
